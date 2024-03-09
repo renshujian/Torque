57e598acce52c5377027b43cb55b462a0139d00f
James Ren <renshujian0@hotmail.com>
2024-03-09 19:03:13 +0800

    添加TickStringConverter, 0.5h

---
+++ Torque/converters/TickStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Torque
+{
+    class TickStringConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is double tick ? TimeSpan.FromTicks((long)tick).ToString() : null;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string s && TimeSpan.TryParse(s, out var timeSpan))
+            {
+                return (double)timeSpan.Ticks;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
