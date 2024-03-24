910e8d8aef9c206b852cc4d6a681920610ea5c9e
James Ren <renshujian0@hotmail.com>
2024-03-24 19:19:46 +0800

    完善TestsViewer, 4h

--- Torque/windows/TestsViewer.xaml.cs
+++ Torque/windows/TestsViewer.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Linq;
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 using System.Windows;
-using Microsoft.EntityFrameworkCore;
 
 namespace Torque
 {
@@ -10,13 +14,60 @@ namespace Torque
     public partial class TestsViewer : Window
     {
         AppDbContext AppDbContext { get; }
+        List<Test> Data { get; set; } = new();
 
         public TestsViewer(AppDbContext appDbContext)
         {
             InitializeComponent();
             AppDbContext = appDbContext;
-            appDbContext.Tests.Load();
-            listView.ItemsSource = appDbContext.Tests.Local.ToObservableCollection().Reverse();
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ToolIdBox.Text = string.Empty;
+            FromDatePicker.SelectedDate = null;
+            ToDatePicker.SelectedDate = null;
+        }
+
+        private void QueryButton_Click(object sender, RoutedEventArgs e)
+        {
+            string toolId = ToolIdBox.Text;
+            DateTime? from = FromDatePicker.SelectedDate;
+            DateTime? to = ToDatePicker.SelectedDate;
+            IEnumerable<Test> tests = AppDbContext.Tests;
+            if (toolId != string.Empty)
+            {
+                tests = tests.Where(it => it.ToolId.Contains(toolId));
+            }
+            if (from != null)
+            {
+                tests = tests.Where(it => it.TestTime >= from);
+            }
+            if (to != null)
+            {
+                tests = tests.Where(it => it.TestTime < to.Value.Date.AddDays(1));
+            }
+            Data = tests.OrderByDescending(it => it.TestTime).ToList();
+            listView.ItemsSource = Data;
+        }
+
+        private void ExportButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new()
+            {
+                DefaultExt = "csv",
+                Filter = "CSV|*.csv",
+            };
+            if (dialog.ShowDialog() == true)
+            {
+                // UTF-8的CSV用Excel打开乱码，UTF-16则不认逗号分隔符，要用TAB分隔
+                using StreamWriter writer = new(File.Create(dialog.FileName), Encoding.Unicode);
+                writer.WriteLine("电批编码\t目标扭矩\t实测扭矩\t允许偏倚\t偏倚\t结果\t检测时间");
+                foreach (var test in Data)
+                {
+                    writer.WriteLine($"{test.ToolId}\t{test.SetTorque}\t{test.RealTorque}\t{test.AllowedDiviation}\t{test.Diviation}\t{test.IsPass}\t{test.TestTime}");
+                }
+            }
         }
     }
 }
